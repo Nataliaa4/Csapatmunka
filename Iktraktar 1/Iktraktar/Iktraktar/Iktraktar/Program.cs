a60ce832f51f92c6e6e88a4a2f94c101e3b1036e
Rendszergazda <rendszergazda@blathy.info>
2025-11-28 13:42:45 +0100

    implement PrintTable

--- Iktraktar 1/Iktraktar/Iktraktar/Iktraktar/Program.cs
+++ Iktraktar 1/Iktraktar/Iktraktar/Iktraktar/Program.cs
@@ -17,7 +17,25 @@ namespace Iktraktar
             storage.Add(new Product(2, "Toll", 50));
             storage.Add(new Product(3, "Füzet", 80));
 
+
+            int id = 0;
+            storage.FindById(id);
+
             Console.WriteLine("Raktár rendszer - fejlesztési alap");
         }
+        
+          public static void PrintTable(IEnumerable<Product> products)
+            {
+                Console.WriteLine("ID | Név     | Készlet");
+                Console.WriteLine("------------------------");
+                foreach (var p in products)
+                {
+                    Console.WriteLine($"{p.Id}  | {p.Name,-7} | {p.Quantity}");
+                }
+            }
+ 
+
+       
     }
 }
+
